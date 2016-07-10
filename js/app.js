f7528f3d39d2d1817199608354bb108048b7d704
tyjow <platon.jonathan@gmail.com>
2016-07-10 10:40:55 +0200

    exemple animation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,31 @@
 var canvas = document.querySelector("#canvas");
 var ctx    = canvas.getContext('2d');
+var posX   = 0;
+var toRight = true;
+var speed  = 3;
 
 canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
+
+function loop() {
+
+	window.requestAnimationFrame(loop);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.beginPath();
+		ctx.fillStyle = '#f26c4f';
+		ctx.rect(posX, canvas.height / 2 - 50, 100, 100);
+		ctx.fill();
+	ctx.closePath();
+
+	if (toRight) 
+		posX+=speed;
+	else
+		posX-=speed;
+
+	if (posX > canvas.width - 100)
+		toRight = false;
+	if (posX < 0)
+		toRight = true;
+}
+
+loop();
