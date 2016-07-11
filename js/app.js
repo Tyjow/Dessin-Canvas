d5b304b6e38fa9ab65449b1ad4d92b5c50e665f3
Tyjow <platon.jonathan@gmail.com>
2016-07-11 17:19:56 +0200

    test animation sprite

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 var canvas = document.querySelector("#canvas");
 var ctx    = canvas.getContext('2d');
+var posX   = 0;
+var toRight = true;
+var speed  = 0.7;
 
 var image = new Image();
-	image.src = 'img/googlelogo.png';
+	image.src = 'img/hero.png';
+
+var height = image.height / 4;
+var width = image.width / 4;
+var frame = 0;
+var count = 0;
 
 canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -11,24 +19,38 @@ function loop() {
 
 	window.requestAnimationFrame(loop);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.drawImage(image, 119, 25, 45, 45, 0, 0, 119, 100);
+	ctx.beginPath();
+		var perso = ctx.drawImage(image, frame * width, 95, width, height, posX, canvas.height / 2 - 50, width, height);
+
+		count++;
+		if (count > 60)
+			count = 0;
+		if (count % 9 == 0)
+			frame++;
+		if (frame > 3)
+			frame = 0;
+
+	ctx.closePath();
+
+	if (toRight) 
+		posX+=speed;
+	else
+		posX-=speed;
+
+	if (posX > canvas.width - width)
+		toRight = false;
+	if (posX < 0)
+		toRight = true;
+	
 }
 
 loop();
 
-	 /*sx = source x
-	   sy = source y
-	   sw = source width
-	   sh = source height
-	   dx = destination x
-	   dy = destination y
-	   dw = destination width
-	   dh = destination height
-	   (obj, sx, sy, sw, sh, dx, dy, dw, dh)*/
+(function resizeCanvas() {
 
-window.onresize = function() {
-	canvas.width  = window.innerWidth;
+	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 
-	console.log('redimension');
-}
+	window.addEventListener("resize", resizeCanvas);
+
+})();
