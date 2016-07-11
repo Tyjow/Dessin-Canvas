ce057828d8128051503a638cf9b0b329ab8e5e68
Tyjow <platon.jonathan@gmail.com>
2016-07-11 14:56:49 +0200

    test de sprites

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var canvas = document.querySelector("#canvas");
 var ctx    = canvas.getContext('2d');
-var posX   = 0;
-var toRight = true;
-var speed  = 3;
+
+var image = new Image();
+	image.src = 'img/googlelogo.png';
 
 canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -11,21 +11,24 @@ function loop() {
 
 	window.requestAnimationFrame(loop);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.beginPath();
-		ctx.fillStyle = '#f26c4f';
-		ctx.rect(posX, canvas.height / 2 - 50, 100, 100);
-		ctx.fill();
-	ctx.closePath();
-
-	if (toRight) 
-		posX+=speed;
-	else
-		posX-=speed;
-
-	if (posX > canvas.width - 100)
-		toRight = false;
-	if (posX < 0)
-		toRight = true;
+	ctx.drawImage(image, 119, 25, 45, 45, 0, 0, 119, 100);
 }
 
 loop();
+
+	 /*sx = source x
+	   sy = source y
+	   sw = source width
+	   sh = source height
+	   dx = destination x
+	   dy = destination y
+	   dw = destination width
+	   dh = destination height
+	   (obj, sx, sy, sw, sh, dx, dy, dw, dh)*/
+
+window.onresize = function() {
+	canvas.width  = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	console.log('redimension');
+}
